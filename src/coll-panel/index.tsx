88e6b4c3fca9aed0f212c1bbf730028839dc96d7
马金花儿 <o.o@mug.dog>
2016-08-26 17:54:11 +0800

    Update index.tsx

--- src/coll-panel/index.tsx
+++ src/coll-panel/index.tsx
@@ -95,7 +95,7 @@ export default class CollPanel extends React.Component <module.PropsInterface, m
                 <div className="panel-heading"
                      onClick={this.handleClick.bind(this)}>
                     <i className={rightChevronClass}
-                       style={{marginRight: '5px'}}/>{header}
+                       style={{marginRight: 5}}/>{header}
                 </div>
                 <div className={panelCollapseClass}
                      style={contentContainerStyle}>
